dab99cd4ef6dd76a22269e6eb6ade2cec3a4abdf
Craiso-HA <christoffer.dahlstrom@ha.ax>
2026-04-14 14:14:06 +0300

    Create AnomalyController.java

---
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/AnomalyController.java
@@ -0,0 +1,22 @@
+package ax.ha.it.cd.ruwich;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/anomalies")
+public class AnomalyController {
+    private final AnomalyRepository anomalyRepository;
+
+    public AnomalyController(AnomalyRepository anomalyRepository) {
+        this.anomalyRepository = anomalyRepository;
+    }
+
+    @GetMapping
+    public List<Anomalies> getAll() {
+        return anomalyRepository.findAll();
+    }
+}
