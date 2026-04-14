b0fa83b769f0b9a59cd6965de0548fe6213c36ca
Craiso-HA <christoffer.dahlstrom@ha.ax>
2026-04-14 13:18:03 +0300

    created project

---
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/RuwichApplication.java
@@ -0,0 +1,13 @@
+package ax.ha.it.cd.ruwich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuwichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuwichApplication.class, args);
+	}
+
+}
---
+++ RUWICH/src/test/java/ax/ha/it/cd/ruwich/RuwichApplicationTests.java
@@ -0,0 +1,13 @@
+package ax.ha.it.cd.ruwich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuwichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
