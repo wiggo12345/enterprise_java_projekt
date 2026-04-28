d132505b9c093be6bd2420706625a5ea2fcaad6d
Craiso-HA <christoffer.dahlstrom@ha.ax>
2026-04-28 14:18:49 +0300

    Created obserationcontroller
    For checking that we can se the observations and trying to create new observations

---
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/ObservationController.java
@@ -0,0 +1,33 @@
+
+package ax.ha.it.cd.ruwich;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+
+
+
+@RestController
+@RequestMapping("/api/observations")
+public class ObservationController {
+    private final ObservationRepository observationRepository;
+
+    public ObservationController(ObservationRepository observationRepository) {
+        this.observationRepository = observationRepository;
+    }
+
+    @GetMapping()
+    public Iterable<Observation> getAll() {
+        return observationRepository.findAll();
+    }
+
+    @PostMapping
+    public Observation addObservation(@RequestBody Observation observation) {
+        return observationRepository.save(observation);
+    }
+}
