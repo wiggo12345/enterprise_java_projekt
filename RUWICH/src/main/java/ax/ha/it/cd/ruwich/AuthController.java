7b8fd1e37fee07b68ded4d557d38315471863624
ruben-ha <ruben.jansson@ha.ax>
2026-04-28 14:48:22 +0300

    securityconfig , auth
    securityconfig for what to be authenticated and controller with a manager for jwt tokens

---
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/AuthController.java
@@ -0,0 +1,36 @@
+package ax.ha.it.cd.ruwich;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
+
+        Authentication auth = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails user = (UserDetails) auth.getPrincipal();
+        return jwtUtil.generateToken(user);
+    }
+}
