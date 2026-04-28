99828eefec89e8d7f52236ef7e44a0beaba5114b
Craiso-HA <christoffer.dahlstrom@ha.ax>
2026-04-28 12:49:08 +0300

    Swapped anomaly to ID in obsrep

--- RUWICH/src/main/java/ax/ha/it/cd/ruwich/ObservationRepository.java
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/ObservationRepository.java
@@ -4,7 +4,7 @@ package ax.ha.it.cd.ruwich;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ObservationRepository extends JpaRepository<Observation, Anomaly> {
+public interface ObservationRepository extends JpaRepository<Observation, Integer> {
 
 
 }
