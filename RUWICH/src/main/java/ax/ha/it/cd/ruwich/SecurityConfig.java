42bf598cb8e560bf67b8f04bb86937fb747c0711
Craiso <200562301+Craiso-HA@users.noreply.github.com>
2026-05-08 09:22:30 +0300

    Added @Profile("!test")  to normal security file
    if test profile is active, it will not use this file

--- RUWICH/src/main/java/ax/ha/it/cd/ruwich/SecurityConfig.java
+++ RUWICH/src/main/java/ax/ha/it/cd/ruwich/SecurityConfig.java
@@ -2,6 +2,7 @@ package ax.ha.it.cd.ruwich;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@Profile("!test")
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
