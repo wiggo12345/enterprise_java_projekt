839b15d9b3a83afea3cd61f83688509c90514c34
Craiso <200562301+Craiso-HA@users.noreply.github.com>
2026-05-08 09:21:40 +0300

    Made a fake testSecConf file for testing environment

---
+++ RUWICH/src/test/java/ax/ha/it/cd/ruwich/TestSecurityConfig.java
@@ -0,0 +1,39 @@
+package ax.ha.it.cd.ruwich;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("test")
+public class TestSecurityConfig {
+
+    @Bean
+    SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return authentication -> authentication;
+    }
+}
